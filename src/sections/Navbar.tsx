827a4ab8a145d45ade935509ba920dba2e998386
Karl Cereno <kagc.dev@gmail.com>
2023-01-08 23:57:51 -0500

    Make mode toggle

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,9 +1,41 @@
-import { Box } from '@mui/material';
+import {
+  Box,
+  ToggleButtonGroup,
+  ToggleButton,
+  createTheme,
+  ThemeProvider,
+} from '@mui/material';
+import { useState } from 'react';
 
 function Navbar() {
+  const [mode, setMode] = useState('lb');
+  console.log('Navbar ~ mode', mode);
+
+  const darkTheme = createTheme({
+    palette: { mode: 'dark' },
+  });
+
+  const handleModeChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newMode: string
+  ) => {
+    if (newMode !== null) {
+      setMode(newMode);
+    }
+  };
   return (
-    <Box className="title text-center py-5 bg-slate-700 text-white mb-8">
+    <Box className="navbar py-5 bg-slate-700 text-white mb-8 flex  justify-around">
       <h1 className="text-3xl">Barbell Calculator</h1>
+      <ThemeProvider theme={darkTheme}>
+        <ToggleButtonGroup
+          value={mode}
+          exclusive
+          onChange={handleModeChange}
+        >
+          <ToggleButton value="lb">lb</ToggleButton>
+          <ToggleButton value="kg">kg</ToggleButton>
+        </ToggleButtonGroup>
+      </ThemeProvider>
     </Box>
   );
 }
